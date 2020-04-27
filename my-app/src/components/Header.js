1990d41a18aedd1b88bb19153fe49b6aaa15ae2f
Sean Kramer <seandominickramer@gmail.com>
2020-04-27 16:01:43 -0400

    Add Home and About components

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,3 +1,22 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Route, Link } from 'react-router-dom';
+// import './index.css';
+
+
+
+const Header = () => {
+
+  return (
+    <nav>
+      <h1>Plant Parenthood</h1>
+      <Link to='/'>Home</Link>
+      <Link to='#'>About</Link>
+      <button id='signup'>Sign Up</button>
+      <button id='login'>Log In</button>
+
+    </nav>
+  )
+}
+
+export default Header;
