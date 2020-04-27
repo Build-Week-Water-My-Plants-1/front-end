7202a2ba5c8158f9313d4a0cbef094f927c423bb
Sean Kramer <seandominickramer@gmail.com>
2020-04-27 15:43:19 -0400

    Add babasic input elements to Login and Signup components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LogIn from './components/Login';
+import SignUp from './components/Signup'
 
 function App() {
   return (
     <div className="App">
+
+      <LogIn />
+      <SignUp />
       
     </div>
   );
---
+++ my-app/src/components/Login.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+
+const LogIn = () => {
+  const [logInData, setLogInData] = useState({
+    username: '',
+    // password: '',
+  })
+  const handleChanges = event => {
+setLogInData({username: event.target.value})
+console.log(logInData)
+  }
+
+  return (
+    <form>
+      <h4>Welcome back!</h4>
+      <h3>Log into your account</h3>
+      <label htmlFor='username'>Username</label>
+      <input id='username' type='text' onChange={handleChanges}/>
+      <label htmlFor='password'>Password</label>
+      <input id='password' type='text'/>
+      <button type='submit'>Next</button>
+    </form>
+  )
+}
+
+export default LogIn;
