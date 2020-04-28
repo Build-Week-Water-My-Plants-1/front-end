f06c38159a890ccf49f0907412b5397255b70a40
Sean Kramer <seandominickramer@gmail.com>
2020-04-28 12:44:40 -0400

    Add properties to input in Login.js and AddPlant.js

--- my-app/src/components/AddPlant.js
+++ my-app/src/components/AddPlant.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
 
 const AddPlant = () => {
   const [addPlantData, setAddPlantData] = useState({
-    plantname: '',
-    // species: '',
-  })
-  const handleChanges = event => {
-    setAddPlantData({plantname: event.target.value})
-    console.log(addPlantData)
-  }
-
+    plantname: "",
+    maintenance: "",
+    species: '',
+  });
+  const handleChanges = (event) => {
+    setAddPlantData({ plantname: event.target.value });
+    console.log(addPlantData);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,31 +24,30 @@ const AddPlant = () => {
     //   .catch((err) => console.log({ err }));
   };
 
-
   return (
     <form>
       <h4>Looking good!</h4>
       <h3>Now let's add your first plant.</h3>
-      <label htmlFor='plantname'>Plant Name</label>
-      <input id='plantname' type='text' onChange={handleChanges}/>
-      <label htmlFor='maintenance'>Maintenance</label>
+      <label htmlFor="plantname">Plant Name</label>
+      <input id="plantname" name='plantname' type="text" placeholder='Plantname' value={addPlantData.plantname} onChange={handleChanges} />
+      <label htmlFor="maintenance">Maintenance</label>
       <select className="size-options" id="maintenance" name="size">
-            <option value="low" onChange={handleChanges}>
-              Low
-            </option>
-            <option value="medium" onChange={handleChanges}>
-              Medium
-            </option>
-            <option value="high" onChange={handleChanges}>
-              High
-            </option>
-          </select>
-      <label htmlFor='species'>Species(optional)</label>
-      <input id='species'/>
-      <button type='submit'>Next</button>
-      <button type='skip'>Skip</button>
+        <option value="low" onChange={handleChanges}>
+          Low
+        </option>
+        <option value="medium" onChange={handleChanges}>
+          Medium
+        </option>
+        <option value="high" onChange={handleChanges}>
+          High
+        </option>
+      </select>
+      <label htmlFor="species">Species(optional)</label>
+      <input id="species" name="species" type="text" placeholder="Species" value={addPlantData.species}/>
+      <button type="submit">Next</button>
+      <button type="skip">Skip</button>
     </form>
-  )
-}
+  );
+};
 
 export default AddPlant;
--- my-app/src/components/Signup.js
+++ my-app/src/components/Signup.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import styled from 'styled-components';
-import {axiosWithAuth} from '../utils/axiosWithAuth'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 // import './index.css';
 
 const SignUp = () => {
   const [signUpData, setSignUpData] = useState({
-    username: '',
-    phonenumber: '',
-    password: '',
-  })
-  const handleChanges = event => {
-    setSignUpData({username: event.target.value})
-    console.log(signUpData)
-  }
-
+    username: "",
+    phonenumber: "",
+    password: "",
+  });
+  const handleChanges = (event) => {
+    setSignUpData({ username: event.target.value });
+    console.log(signUpData);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,22 +27,40 @@ const SignUp = () => {
       .catch((err) => console.log({ err }));
   };
 
-
   return (
     <form>
       <h4>Let's get started!</h4>
       <h3>Create your account</h3>
-      <label htmlFor='username'>Username</label>
-      <input id='username' name='username' type='text' placeholder='Username' value={signUpDate.username} onChange={handleChanges}/>
-      <label htmlFor='phonenumber'>Phone Number</label>
-      <input id='phonenumber' name='phonenumber' type='text' placeholder='PhoneNumber' value={signUpData.phonenumber}/>
-      <label htmlFor='password'>Password</label>
-      <input id='password' name='password' type='text' placeholder='Password' value={signUpData.password}/>
-      <label htmlFor='confirmpw'>Confirm Password</label>
-      <input id='confirmpw'/>
-      <button type='submit'>Next</button>
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        name="username"
+        type="text"
+        placeholder="Username"
+        value={signUpData.username}
+        onChange={handleChanges}
+      />
+      <label htmlFor="phonenumber">Phone Number</label>
+      <input
+        id="phonenumber"
+        name="phonenumber"
+        type="text"
+        placeholder="PhoneNumber"
+        value={signUpData.phonenumber}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="text"
+        placeholder="Password"
+        value={signUpData.password}
+      />
+      {/* <label htmlFor='confirmpw'>Confirm Password</label>
+      <input id='confirmpw'/> */}
+      <button type="submit">Next</button>
     </form>
-  )
-}
+  );
+};
 
 export default SignUp;
