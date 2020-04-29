6e1827b8c7f630f946f217fe117d998cd2fd84e7
Sean Kramer <seandominickramer@gmail.com>
2020-04-29 13:17:33 -0400

    Add axios get request to fetchPlants function

--- my-app/src/action/action.js
+++
@@ -1,5 +0,0 @@
-export const getID = (id) => {
-  //console.log("item",item)
-  // dipsatch an action here to add an item
-  return { type: "GET_ID", id: id };
-};
---
+++ my-app/src/action/index.js
@@ -0,0 +1,23 @@
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+
+export const fetchPlants = (props) => dispatch => {
+  dispatch({ type: 'FETCHING_PLANTS' })
+  axiosWithAuth()
+  .get(`/api/${props.id}/plants`)
+  .then(res => addPlantData(res.data))
+  .catch(err => console.log(err))
+}
+
+export const getID = id => {
+  return { type: "GET_ID", id: id };
+};
+
+export const deletePlant = id => {
+  return { type: 'DELETE_PLANT', payload: id }
+}
+
+export const plantUpdate = task => {
+  return { type: 'UPDATE_PLANT', payload: task}
+}
+
+
