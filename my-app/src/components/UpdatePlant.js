a9569208363b9f05dadc751ac7dde8c041e86322
janecyyu <janecyyu@gmail.com>
2020-04-29 13:55:26 -0700

    get update data

--- my-app/src/components/UpdatePlant.js
+++ my-app/src/components/UpdatePlant.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { getPlantID, getID } from "../action/index";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const initialItem = {
+  common_name: "",
+  scientific_name: "",
+  h2o_frequency: "",
+};
 
 const UpdatePlate = (props) => {
-    // console.log(props.plantID)
+  // console.log(props.plantID)
+  const [plant, setPlant] = useState(initialItem);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/api/${props.id}/plants/${props.plantID}`)
+      .then((res) => setPlant(res.data))
+      .catch((err) => console.log(err));
+  }, [props.plantID]);
   return (
     <div>
       <h1>Update Plants</h1>
-      <div>{props.plantID}</div>
+      <form>
+        <input
+          type="text"
+          name="name"
+          //   onChange={changeHandler}
+          placeholder="name"
+          value={plant.common_name}
+        />
+        <br />
+        <input
+          type="number"
+          name="frequency"
+          //   onChange={changeHandler}
+          placeholder="frequency"
+          value={plant.h2o_frequency}
+        />
+        <div className="baseline" />
+        <input
+          type="text"
+          name="scientific_name"
+          //   onChange={changeHandler}
+          placeholder="scientific_name"
+          value={plant.scientific_name}
+        />
+        <div className="baseline" />
+      </form>
     </div>
   );
 };
@@ -15,7 +55,7 @@ const mapStateToProps = (state) => {
   //console.log("map at update", state);
   return {
     id: state[0].id,
-    plantID:state.plantID
+    plantID: state.plantID,
   };
 };
 export default connect(mapStateToProps, { getID, getPlantID })(UpdatePlate);
