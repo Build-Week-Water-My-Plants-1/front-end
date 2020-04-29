e56ea61a8533517e38ef622fa40a68d5de0ee346
janecyyu <janecyyu@gmail.com>
2020-04-29 13:21:11 -0700

    get plant id

--- my-app/src/components/PlantDashboard.js
+++ my-app/src/components/PlantDashboard.js
@@ -1,17 +1,38 @@
-import React, { useState } from 'react'
-import PlantList from './PlantList'
+import React, { useState, useEffect } from "react";
+// import { fetchPlants } from "../action";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { connect } from "react-redux";
+import { getID, getPlantID } from "../action";
+import { useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import PlantList from "./PlantList";
 
-const plantDashboard = () => {
+const PlantDashBoard = (props) => {
+  console.log(props);
+  const [plant, setPlant] = useState([]);
 
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/api/${props.id}/plants`)
+      .then((res) => setPlant(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(plant);
+  return (
+    <div>
+      <h1>Hello plants dashboard!</h1>
+      {plant.map((p) => (
+        <PlantList plant={p} />
+      ))}
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <PlantList />
-        </div>
-        
-    )
-}
-
-export default plantDashboard;
-
-
+const mapStateToProps = (state) => {
+  console.log("map at plant list:", state[0]);
+  return {
+    id: state[0].id,
+    plantID: state[0].plantID,
+  };
+};
+export default connect(mapStateToProps, { getID, getPlantID })(PlantDashBoard);
--- my-app/src/components/PlantList.js
+++ my-app/src/components/PlantList.js
@@ -1,38 +1,26 @@
-import React, { useState } from "react";
-// import { fetchPlants } from "../action";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { getID, getPlantID } from "../action";
 import { useHistory } from "react-router-dom";
 
 const PlantList = (props) => {
-  const { push } = useHistory();
-  const [plant, setPlant] = useState([]);
-  axiosWithAuth()
-    .get(`/api/${props.id}/plants`)
-    .then((res) => setPlant(res.data))
-    .catch((err) => console.log(err));
+  //console.log("props",props.getPlantID(props.plant.id));
+  //props.plantID(props.plant.id)
+  const {push} = useHistory()
+  props.getPlantID(props.plant.id);
   return (
     <div>
-      <h1>Hello plants!</h1>
-      {plant.map((p) => (
-        
-        <div>
-          <h3>Name: {p.common_name}</h3>
-          <h3>H2o_frequency: {p.h2o_frequency}</h3>
-          <h3>Species: {p.scientific_name}</h3>
-          <button onClick={() => push("/update")}>Edit</button>
-          <br />
-        </div>
-      ))}
+      <h1>{props.plant.common_name}</h1>
+      <button onClick={()=>push('./update')}>Edit</button>
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
-  console.log("map at plant list:", state[0]);
+  //console.log("map at plant list:", state[0]);
   return {
     id: state[0].id,
+    plantID: state[0].plantID,
   };
 };
 export default connect(mapStateToProps, { getID, getPlantID })(PlantList);
